3bdbe510001a0b31b92c084158801c4f58452a86
Francisco | DevFran25 <developerfran25@gmail.com>
2026-01-30 22:08:27 -0500

    Se agrego UsuarioRepository y AutenticacionService

---
+++ src/main/java/com/topico/api/domain/usuario/AutenticacionService.java
@@ -0,0 +1,23 @@
+package com.topico.api.domain.usuario;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AutenticacionService implements UserDetailsService {
+
+    @Autowired
+    private UsuarioRepository repository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username)
+            throws UsernameNotFoundException {
+
+        return repository.findByUsername(username)
+                .orElseThrow(() ->
+                        new UsernameNotFoundException("Usuario no encontrado"));
+    }
+}
